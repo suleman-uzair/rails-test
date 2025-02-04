c81fd86a53ef8562c41276c589abac2124f68267
suleman-uzair <suleman@workytical.com>
2025-02-04 18:43:33 +0500

    testing workflow

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -3,6 +3,5 @@ require "test_helper"
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "the truth" do
     assert_equal "a s", Plurimath::Math.parse("as", :asciimath).to_asciimath
-    assert_equal "a s", Plurimath::Math.parse("as", :asciimath).to_asciimath
   end
 end
